60487a01571f5ffdb1475b60ee9a1bd8142dfec3
Md Irfan <work.mdirfan@gmail.com>
2025-06-08 23:23:48 +0530

    Footer removed Update Comming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Outlet } from 'react-router'
-import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -10,7 +9,6 @@ const App = () => {
       <main style={{minHeight: "100vh"}}>
           <Outlet />
       </main>
-      <Footer />
     </>
   )
 }
