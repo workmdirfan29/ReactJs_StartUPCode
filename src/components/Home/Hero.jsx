48d2029289f853da4fe3780b84837c8e3625de18
Md Irfan <work.mdirfan@gmail.com>
2025-06-09 23:50:05 +0530

    Landing page Comming sson

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div style={{backgroundColor : "green", height: "100vh"}}>
-      Hero
+     Landing page will be comming soon !
     </div>
   )
 }
